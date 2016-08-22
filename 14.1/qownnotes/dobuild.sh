ee966d65991829c20375d5236906dad7a3cc5d44
Patrizio Bekerle <office@bekerle.com>
2016-08-22 13:51:35 +0200

    releasing version 16.08.17

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 16.08.16
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-16.08.16.tar.xz"
+# src for version 16.08.17
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-16.08.17.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
