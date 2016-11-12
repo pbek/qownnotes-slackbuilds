3dee71cb5b6e29b19959915d9d690954341132d5
Patrizio Bekerle <patrizio@bekerle.com>
2016-11-12 13:30:57 +0100

    releasing version 16.11.8

--- 14.2/qownnotes/dobuild.sh
+++ 14.2/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 16.11.7
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-16.11.7.tar.xz"
+# src for version 16.11.8
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-16.11.8.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
