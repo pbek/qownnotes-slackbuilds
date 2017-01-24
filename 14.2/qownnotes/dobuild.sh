501dc9c6eaea405d1a46a470ff4de2a78094fd59
Patrizio Bekerle <patrizio@bekerle.com>
2017-01-24 17:59:46 +0100

    releasing version 17.01.9

--- 14.2/qownnotes/dobuild.sh
+++ 14.2/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 17.01.8
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.01.8.tar.xz"
+# src for version 17.01.9
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.01.9.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
