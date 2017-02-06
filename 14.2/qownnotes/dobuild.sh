e8782b5564491e1c58c19ac662b8cfc892e28ef6
Patrizio Bekerle <patrizio@bekerle.com>
2017-02-06 19:49:54 +0100

    releasing version 17.02.3

--- 14.2/qownnotes/dobuild.sh
+++ 14.2/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 17.02.2
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.02.2.tar.xz"
+# src for version 17.02.3
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.02.3.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
