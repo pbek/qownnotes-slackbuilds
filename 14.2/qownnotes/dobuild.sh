5cc23ed1ee79cfefd7c91045495084b59fa1fd4e
Patrizio Bekerle <patrizio@bekerle.com>
2017-05-29 18:05:54 +0200

    releasing version 17.05.13

--- 14.2/qownnotes/dobuild.sh
+++ 14.2/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 17.05.12
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.05.12.tar.xz"
+# src for version 17.05.13
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.05.13.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
