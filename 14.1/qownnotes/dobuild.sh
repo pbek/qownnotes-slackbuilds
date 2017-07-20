883fe5eb56305d830b5e6cde2b9d001e86a691f9
Patrizio Bekerle <patrizio@bekerle.com>
2017-07-20 18:04:37 +0200

    releasing version 17.07.5

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 17.07.4
-wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.07.4.tar.xz"
+# src for version 17.07.5
+wget "http://downloads.sourceforge.net/project/qownnotes/src/qownnotes-17.07.5.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
