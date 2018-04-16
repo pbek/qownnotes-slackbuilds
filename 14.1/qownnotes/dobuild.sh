a1b914bd988bbc79c3df42e1903be90035c71a94
Patrizio Bekerle <patrizio@bekerle.com>
2018-04-16 17:33:38 +0200

    releasing version 18.04.3

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 18.04.2
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.04.2.tar.xz"
+# src for version 18.04.3
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.04.3.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
