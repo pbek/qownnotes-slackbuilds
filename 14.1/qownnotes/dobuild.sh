fecdf18ba6696c8beb928820bda45354223c9493
Patrizio Bekerle <patrizio@bekerle.com>
2018-08-26 12:00:20 +0200

    releasing version 18.08.9

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 18.08.8
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.08.8.tar.xz"
+# src for version 18.08.9
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.08.9.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
