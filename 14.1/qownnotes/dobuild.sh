55e08332b980d8921225146c7401f0d05c6ea3c8
Patrizio Bekerle <patrizio@bekerle.com>
2018-12-27 12:55:54 +0100

    releasing version 18.12.7

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 18.12.6
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.12.6.tar.xz"
+# src for version 18.12.7
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-18.12.7.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
