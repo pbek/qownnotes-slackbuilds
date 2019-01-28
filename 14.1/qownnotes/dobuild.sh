03d9fbb6399a49c861bff352f112ba52d77ba10b
Patrizio Bekerle <patrizio@bekerle.com>
2019-01-28 19:15:03 +0100

    releasing version 19.1.11

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 19.1.10
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-19.1.10.tar.xz"
+# src for version 19.1.11
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-19.1.11.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
