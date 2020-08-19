f462174784c18cae5ce5946c78e518b9c9eb9306
Patrizio Bekerle <patrizio@bekerle.com>
2020-08-19 18:56:31 +0200

    releasing version 20.8.7

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 20.8.6
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-20.8.6.tar.xz"
+# src for version 20.8.7
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-20.8.7.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
