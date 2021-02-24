ee97fc3258f0df196ddcbd070bc93efcfc12f2d4
Patrizio Bekerle <patrizio@bekerle.com>
2021-02-24 22:21:41 +0100

    releasing version 21.2.4

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 21.2.3
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.2.3.tar.xz"
+# src for version 21.2.4
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.2.4.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
