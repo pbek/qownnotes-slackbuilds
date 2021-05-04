c3a3497944c78c44b1ffd3a37e2ee346800a91c7
Patrizio Bekerle <patrizio@bekerle.com>
2021-05-04 18:56:50 +0200

    releasing version 21.5.0

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 21.4.6
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.4.6.tar.xz"
+# src for version 21.5.0
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.5.0.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
