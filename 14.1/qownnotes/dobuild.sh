ea2f19cab68cd00377926b01ef4e6e5ed7e97274
Patrizio Bekerle <patrizio@bekerle.com>
2021-11-24 10:44:24 +0100

    releasing version 21.11.10

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 21.11.9
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.11.9.tar.xz"
+# src for version 21.11.10
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-21.11.10.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
