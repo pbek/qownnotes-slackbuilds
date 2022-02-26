946dc92ced884c10c615ce4f7066be18099ad918
Patrizio Bekerle <patrizio@bekerle.com>
2022-02-26 13:48:39 +0100

    releasing version 22.2.9

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 22.2.8
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-22.2.8.tar.xz"
+# src for version 22.2.9
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-22.2.9.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
