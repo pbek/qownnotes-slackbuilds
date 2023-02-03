fcfb01e7e5b77fe894c0f60695be27b174f1bbe1
Patrizio Bekerle <patrizio@bekerle.com>
2023-02-03 17:11:16 +0100

    releasing version 23.2.0

--- 14.2/qownnotes/dobuild.sh
+++ 14.2/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 23.1.6
-wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-23.1.6.tar.xz"
+# src for version 23.2.0
+wget "https://download.tuxfamily.org/qownnotes/src/qownnotes-23.2.0.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
