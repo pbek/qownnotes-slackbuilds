2d8fc49a43b941a1b609c861d76014868f14a7d8
Patrizio Bekerle <patrizio@bekerle.com>
2023-09-14 15:37:47 +0200

    releasing version 23.9.3

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 23.9.2
-wget "https://github.com/pbek/QOwnNotes/releases/download/v23.9.2/qownnotes-23.9.2.tar.xz"
+# src for version 23.9.3
+wget "https://github.com/pbek/QOwnNotes/releases/download/v23.9.3/qownnotes-23.9.3.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
