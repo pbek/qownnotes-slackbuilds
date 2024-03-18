f271304338cfc994ecf471ce63b9c2d1fdc7d0bf
Patrizio Bekerle <patrizio@bekerle.com>
2024-03-18 19:37:02 +0100

    releasing version 24.3.4

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 24.3.3
-wget "https://github.com/pbek/QOwnNotes/releases/download/v24.3.3/qownnotes-24.3.3.tar.xz"
+# src for version 24.3.4
+wget "https://github.com/pbek/QOwnNotes/releases/download/v24.3.4/qownnotes-24.3.4.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
