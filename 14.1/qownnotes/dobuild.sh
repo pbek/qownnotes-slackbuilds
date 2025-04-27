84a7566a03f1566aefd21f7795f2591c915c9044
Patrizio Bekerle <patrizio@bekerle.com>
2025-04-27 14:06:56 +0200

    releasing version 25.4.5

--- 14.1/qownnotes/dobuild.sh
+++ 14.1/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 25.4.4
-wget "https://github.com/pbek/QOwnNotes/releases/download/v25.4.4/qownnotes-25.4.4.tar.xz"
+# src for version 25.4.5
+wget "https://github.com/pbek/QOwnNotes/releases/download/v25.4.5/qownnotes-25.4.5.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
