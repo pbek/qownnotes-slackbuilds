a8f14fd584fa7639dd1deb673f16d298ba9443a7
Patrizio Bekerle <patrizio@bekerle.com>
2026-01-21 20:40:24 +0100

    releasing version 26.1.10

--- 15.0/qownnotes/dobuild.sh
+++ 15.0/qownnotes/dobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# src for version 26.1.9
-wget "https://github.com/pbek/QOwnNotes/releases/download/v26.1.9/qownnotes-26.1.9.tar.xz"
+# src for version 26.1.10
+wget "https://github.com/pbek/QOwnNotes/releases/download/v26.1.10/qownnotes-26.1.10.tar.xz"
 
 # run the build script
 ./qownnotes.SlackBuild
